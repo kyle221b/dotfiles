cbdff1daffc0beaadc7d188180534dffd648b8cb
kyle221b <kyle221b@gmail.com>
2024-05-25 13:10:35 -0700

    neovim updates

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -1,134 +1,109 @@
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = true,
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = true,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "folke/neodev.nvim", opts = {} },
+			{ "j-hui/fidget.nvim" },
+		},
+		config = function()
+			local telescope = require("telescope.builtin")
 
-  -- Autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "L3MON4D3/LuaSnip" },
-      { "rafamadriz/friendly-snippets" },
-    },
-    config = function()
-      -- Here is where you configure the autocompletion settings.
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_cmp()
+			vim.api.nvim_create_autocmd("BufWrite", {
+				desc = "Format on save",
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
 
-      -- And you can configure cmp even more, if you want to.
-      local cmp = require("cmp")
-      local cmp_action = lsp_zero.cmp_action()
+			-- note: diagnostics are not exclusive to lsp servers
+			-- so these can be global keybindings
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
-      cmp.setup({
-        sources = {
-          { name = "path" },
-          { name = "nvim_lsp" },
-          { name = "nvim_lua" },
-        },
-        formatting = lsp_zero.cmp_format(),
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-        }),
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local bufnr = event.buf
+					local opts = { buffer = bufnr }
 
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+					-- these will be buffer-local keybindings
+					-- because they only work if you have an active language server
 
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
-    end,
-  },
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    cmd = { "LspInfo", "LspInstall", "LspStart" },
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "williamboman/mason-lspconfig.nvim" },
-      { "folke/neodev.nvim",                opts = {} },
-      { "j-hui/fidget.nvim" },
-    },
-    config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
+					-- Use telescope for a lot of the lsp lookups
+					vim.keymap.set("n", "gd", telescope.lsp_definitions, { buffer = bufnr, desc = "Go to definition" })
+					vim.keymap.set(
+						"n",
+						"gi",
+						telescope.lsp_implementations,
+						{ buffer = bufnr, desc = "Go to implementations" }
+					)
+					vim.keymap.set("n", "gr", telescope.lsp_references, { buffer = bufnr, desc = "Go to references" })
+					vim.keymap.set(
+						"n",
+						"gt",
+						telescope.lsp_type_definitions,
+						{ buffer = bufnr, desc = "Go to type definitions" }
+					)
+					vim.keymap.set(
+						{ "n", "i" },
+						"<C-k>",
+						vim.lsp.buf.signature_help,
+						{ buffer = bufnr, desc = "Signature help" }
+					)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+					vim.keymap.set({ "n", "x" }, "<leader><C-f>", function()
+						vim.lsp.buf.format({ async = true })
+					end, { buffer = bufnr, desc = "Format" })
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				end,
+			})
 
-      local telescope = require("telescope.builtin")
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
+			local default_setup = function(server)
+				require("lspconfig")[server].setup({
+					capabilities = lsp_capabilities,
+				})
+			end
 
-        -- Custom definitions / overrides
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
-        vim.keymap.set(
-          { "n", "i" },
-          "<C-k>",
-          vim.lsp.buf.signature_help,
-          { buffer = bufnr, desc = "Signature help" }
-        )
-
-        -- Use telescope for a lot of the lsp lookups
-        vim.keymap.set("n", "gd", telescope.lsp_definitions, { buffer = bufnr, desc = "Go to definition" })
-        vim.keymap.set(
-          "n",
-          "gi",
-          telescope.lsp_implementations,
-          { buffer = bufnr, desc = "Go to implementations" }
-        )
-        vim.keymap.set("n", "gr", telescope.lsp_references, { buffer = bufnr, desc = "Go to references" })
-      end)
-
-      -- local lspconfig = require("lspconfig")
-      require("mason-lspconfig").setup({
-        ensure_installed = { "tsserver", "lua_ls", "rust_analyzer", "gopls", "kotlin_language_server" },
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            -- (Optional) Configure lua language server for neovim
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
-          end,
-          -- tsserver = function()
-          -- 	lspconfig.tsserver.setup({
-          -- 		settings = {
-          -- 			root_dir = lspconfig.util.root_pattern("tsconfig.json"),
-          -- 		},
-          -- 	})
-          -- end,
-        },
-      })
-    end,
-  },
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "tsserver", "lua_ls" },
+				handlers = {
+					default_setup,
+					lua_ls = function()
+						require("lspconfig").lua_ls.setup({
+							capabilities = lsp_capabilities,
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME,
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 }
