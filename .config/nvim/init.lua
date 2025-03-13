7a6a90f4e434671b329561a85b0c472aea637903
kyle221b <kyle221b@gmail.com>
2025-03-13 11:26:55 -0700

    Add vscode lua config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,21 @@
 require("config/remap")
 require("config/options")
-require("config/lazy")
 
-vim.cmd('colorscheme gruvbox')
-vim.cmd [[ highlight Normal guibg=NONE ]]
+if vim.g.vscode then
+  require("config/vscode")
+else
+  require("config/lazy")
 
-local function load_project_config()
-  local project_config = vim.fs.find('.nvim.lua', { upward = true, type = 'file' })
-  if #project_config > 0 then
-    local config_path = project_config[1]
-    dofile(config_path)
+  vim.cmd('colorscheme gruvbox')
+  vim.cmd [[ highlight Normal guibg=NONE ]]
+
+  local function load_project_config()
+    local project_config = vim.fs.find('.nvim.lua', { upward = true, type = 'file' })
+    if #project_config > 0 then
+      local config_path = project_config[1]
+      dofile(config_path)
+    end
   end
-end
 
-load_project_config()
+  load_project_config()
+end
---
+++ .config/nvim/lua/config/vscode.lua
@@ -0,0 +1,10 @@
+vim.opt.relativenumber = true
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
